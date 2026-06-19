5401fac18f05195db5fdb1056797f47d09ccc5c4
Yeahia <yeahianurulamin@gmail.com>
2026-06-20 00:01:25 +0300

    Add Clerk webhook handling and update environment example
    - Introduced Clerk webhook middleware to process user events (created, updated, deleted).
    - Updated environment example file to include Clerk API keys.
    - Renamed `clorkID` to `clerkId` in the User model for consistency.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clorkID: {
+    clerkId: {
         type: String,
         required: true,
         unique: true,
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import { startCronJobs } from "./lib/cron.js";
 import { clerkMiddleware } from '@clerk/express'
 import fs from "fs";
 import path from "path";
-// import { clerkWebhookMiddleware } from "./webhooks/clerkWebhook.js";
+import clerkWebhookRouter from "./webhooks/clerkWebhookMiddleware.js";
 
 
 // Initialize Express app
@@ -22,7 +22,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 // Middleware
 // Set up Clerk webhook middleware
-// app.use("/api/webhooks/clerk", express.raw({type: "application/json"}), clerkWebhookMiddleware()); // express.raw is keeps the body raw and not parsed
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhookRouter);
 // Set up CORS and JSON middleware
 app.use(cors({
   origin: FRONTEND_URL,
