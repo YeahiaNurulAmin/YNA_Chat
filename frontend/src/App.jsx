830bc4095298ed114b397092076e5b50d7c1a4a3
Yeahia <yeahianurulamin@gmail.com>
2026-06-21 22:31:37 +0300

    Enhance frontend with new features and dependencies
    - Updated package.json and package-lock.json to include new dependencies: @heroui/react, @heroui/styles, @tailwindcss/vite, axios, lucide-react, react-hot-toast, react-router, socket.io-client, tailwindcss, and zustand.
    - Integrated Tailwind CSS and HeroUI styles into the Vite configuration.
    - Added new components for theme and wallpaper selection, including ThemePresetPicker, ThemeToggle, and WallpaperPicker.
    - Implemented loading and authentication UI components: PageLoader and AuthActionPanel.
    - Introduced new assets including logos, wallpapers, and sound effects.
    - Removed unused App.css and assets to streamline the project structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,25 @@
-import {
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/react'
-import './App.css'
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+
 
 export default function App() {
-  return (
-    <>
-      <header className="auth-header">
-        <h1>YNA Chat</h1>
-        <div className="auth-actions">
-          <Show when="signed-out">
-            <SignInButton mode="modal"/>
-            <SignUpButton mode="modal"/>
-          </Show>
-          <Show when="signed-in">
-            <UserButton />
-          </Show>
-        </div>
-      </header>
+  const { isSignedIn } = useAuth();
 
-      <main className="chat-placeholder">
-        <p>Sign in to start chatting.</p>
-      </main>
-    </>
+  return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/react'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider afterSignOutUrl="/">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
 )
